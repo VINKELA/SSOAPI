ea0a8e41dc97dbcfa81ed2c0cf62542ca51b1fca
orjikalu <kelvinkaluorji@yahoo.com>
2022-07-06 10:11:19 +0100

    add db context to start up

--- SSOMachine/Startup.cs
+++ SSOMachine/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SSOService.Models.Domains;
 
 namespace SSOMachine
 {
@@ -18,7 +20,7 @@ namespace SSOMachine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SchoolContext>(options =>
+            services.AddDbContext<DBContext>(options =>
                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
